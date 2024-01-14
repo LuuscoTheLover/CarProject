2b8f5e0500189ae45dea57598354181ac84c0132
LuuscoTheLover <127765876+LuuscoTheLover@users.noreply.github.com>
2024-01-14 20:06:36 -0300

    Rear Camera Fixed
    now fix the wheels

--- Scripts/Camera.gd
+++ Scripts/Camera.gd
@@ -9,16 +9,15 @@ func _ready():
 	look = car.global_position
 
 func _process(delta):
-	if Input.is_action_pressed("accelerate"):
-		current = true
-	elif car.linear_velocity.dot(car.basis.z) > -1 and Input.is_action_pressed("reverse"):
+	if car.linear_velocity.dot(car.basis.z) > 0:
 		$"../Rear Camera".current = true
-	elif car.linear_velocity.dot(car.basis.z) > 0:
-		$"../Rear Camera".current = true
-	elif car.linear_velocity.dot(car.basis.z) < 0:
+	else:
 		current = true
+
 	if current:
 		pivot.global_position = lerp(pivot.global_position, car.global_position, delta * 20.0)
-		pivot.rotation.y = lerp_angle(pivot.rotation.y, car.rotation.y, delta * 5.0)
-		look = lerp(look, car.global_position + Vector3(car.linear_velocity.x * direction_offset, car.linear_velocity.y, car.linear_velocity.z) , delta * 5.0)
+		pivot.rotation.y = lerp_angle(pivot.rotation.y, car.rotation.y, delta * direction_offset)
+		look = lerp(look, car.global_position + Vector3(car.linear_velocity.x, car.linear_velocity.y, car.linear_velocity.z), delta * 20)
 		look_at(look)
+		rotation_degrees.x = clamp(rotation_degrees.x, -8, -10)
+		rotation_degrees.y = clamp(rotation_degrees.x, -90, 90)
--- Scripts/Rear Camera.gd
+++ Scripts/Rear Camera.gd
@@ -3,7 +3,6 @@ extends Camera3D
 
 @onready var car = $"../.."
 @onready var pivot = $".."
-@export var direction_offset : float = 1.0
 
 var look : Vector3
 func _ready():
@@ -12,6 +11,8 @@ func _ready():
 func _process(delta):
 	if current:
 		pivot.global_position = lerp(pivot.global_position, car.global_position, delta * 20.0)
-		pivot.rotation.y = lerp_angle(pivot.rotation.y, car.rotation.y, delta * 5.0)
-		look = lerp(look, car.global_position + Vector3(car.linear_velocity.x * direction_offset, car.linear_velocity.y, car.linear_velocity.z) , delta * 5.0)
+		pivot.rotation.y = lerp_angle(pivot.rotation.y, car.rotation.y, delta * $"../Camera".direction_offset)
+		look = lerp(look, car.global_position + car.linear_velocity, delta * 20)
 		look_at(look)
+		rotation_degrees.x = clamp(rotation_degrees.x, -8, -10)
+		rotation_degrees.y = clamp(rotation_degrees.x, 180, 270)
