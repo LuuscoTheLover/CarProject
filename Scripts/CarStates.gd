c2357498b53a6e1767252cebd9aa2bd4cdfe9ecd
LuuscoTheLover <127765876+LuuscoTheLover@users.noreply.github.com>
2024-01-20 00:50:33 -0300

    started adding states machines

---
+++ Scripts/CarStates.gd
@@ -0,0 +1,21 @@
+class_name CarStates
+extends Node
+
+@export var car : CarScript
+
+var state : State
+
+func enter_state(new_state : State):
+	pass
+
+
+func exit_state(state):
+	pass
+	
+
+func state_process(delta):
+	pass
+	
+
+func state_physics_process(delta):
+	pass
