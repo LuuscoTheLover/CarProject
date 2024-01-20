a5382bc08b4a15ea7052e32cd7dcfc82c712c980
LuuscoTheLover <127765876+LuuscoTheLover@users.noreply.github.com>
2024-01-20 13:34:02 -0300

    state mahines progressing

---
+++ Scripts/FiniteStateMachine.gd
@@ -0,0 +1,29 @@
+class_name FiniteStateMachine
+extends Node
+
+var current_state : State
+var states : Dictionary = {}
+@export var initial_state : State
+
+func _ready():
+	for child in get_children():
+		if child is State:
+			states[child.name.to_lower()] = child
+			child.state_trasition.connect(change_state)
+	
+	if initial_state:
+		initial_state.enter()
+		current_state = initial_state
+
+
+func change_state(old_state : State, new_state_name : String):
+	if old_state != current_state:
+		push_error("old state is: %s, and current state is: %s" % [old_state.name, current_state.name])
+		return
+	var new_state = states.get(new_state_name.to_lower())
+	if not new_state:
+		push_error("new state is empty")
+		return
+	current_state.exit()
+	new_state.enter()
+	current_state = new_state
--- Scripts/State.gd
+++ Scripts/State.gd
@@ -1,2 +1,22 @@
-class_name State
 extends Node
+class_name State
+signal state_trasition
+
+@export var car : CarScript
+
+var state : State
+
+func enter():
+	pass
+
+
+func exit():
+	pass
+	
+
+func state_process(delta):
+	pass
+	
+
+func state_physics_process(delta):
+	pass
